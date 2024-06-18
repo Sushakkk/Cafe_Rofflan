8338a3177ebd08077a3eb8a9afae8bcc7c8e79a4
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-06-18 19:12:22 +0300

    Общая структура сайта кафе без моделей

--- Coffee_shop/coffee_shop/coffee_shop/settings.py
+++ Coffee_shop/coffee_shop/coffee_shop/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'goods',
+    'baskets',
+    'users',
+    'orders',
+
 ]
 
 MIDDLEWARE = [
@@ -117,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+    ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
---
+++ Coffee_shop/coffee_shop/main/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+
+
+
+def index(request):
+
+    context = {
+
+        'title': 'Home - Главная',
+
+        'content': "Магазин мебели HOME"
+
+
+
+
+    }
+
+
+
+
+    return render(request, 'main/index.html', context)
+
+
+
+def about(request):
+
+    context = {
+
+        'title': 'Home - О нас',
+
+        'content': "О нас",
+
+        'text_on_page': "Текст о том почему этот магазин такой классный, и какой хороший товар."
+
+    }
+
+
+
+
+    return render(request, 'main/about.html', context)
