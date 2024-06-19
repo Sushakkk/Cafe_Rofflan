88b5d975ff2d45a6cc7bdc41c4ca63fbedc8236c
Карпова Ксения <ksenia.karpova.4@mail.ru>
2024-06-19 15:07:51 +0300

    доделала маршруты в goods

--- Coffee_shop/coffee_shop/goods/urls.py
+++ Coffee_shop/coffee_shop/goods/urls.py
@@ -6,5 +6,5 @@ app_name = 'goods'
 
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('dish/<slug:product_slug>/', views.dish, name='dish'),
+    path('dish/<slug:dish_slug>/', views.dish, name='dish'),
 ]
