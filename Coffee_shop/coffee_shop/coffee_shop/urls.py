5307ac2dd04fd767634e60f624c25c6128b517d3
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-07-02 13:25:47 +0300

    Подключили фронтенд приложений main и goods. Отключены старые стили для остальных приложений

--- Coffee_shop/coffee_shop/coffee_shop/urls.py
+++ Coffee_shop/coffee_shop/coffee_shop/urls.py
@@ -26,7 +26,7 @@ from coffee_shop import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('menu/', include('goods.urls', namespace='catalog')),
+    path('menu/', include('goods.urls', namespace='menu')),
     path('user/', include('users.urls', namespace='user')),
 
     path('__debug__/', include('debug_toolbar.urls')),
