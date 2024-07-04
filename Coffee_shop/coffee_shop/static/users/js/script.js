eedc90310bbf6ff5e5f79659ec9e14e757775615
Дмитрий Радченко <dimaradchenko270304@yandex.ru>
2024-07-04 13:10:54 +0300

    Автообновление регистрации и авторизации , создание уведомлений на страницах(еще не подключены)

--- Coffee_shop/coffee_shop/static/users/js/script.js
+++ Coffee_shop/coffee_shop/static/users/js/script.js
@@ -1,9 +1,31 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+// const signUpButton = document.getElementById('signUp');
+// const signInButton = document.getElementById('signIn');
+// const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () =>
-container.classList.add('right-panel-active'));
+// signUpButton.addEventListener('click', () =>
+// container.classList.add('right-panel-active'));
 
-signInButton.addEventListener('click', () =>
-container.classList.remove('right-panel-active'));
+// signInButton.addEventListener('click', () =>
+// container.classList.remove('right-panel-active'));
+
+document.addEventListener("DOMContentLoaded", () => { //Ждем загрузки всей HTML-разметки перед выполнением кода.
+    const signUpButton = document.getElementById('signUp');//Инициализация элементов
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
+
+    // Проверка состояния при загрузке страницы
+    if (localStorage.getItem('rightPanelActive') === 'true') { //Если значение в localStorage равно 'true',
+                                                              // добавляем класс right-panel-active к контейнеру.
+        container.classList.add('right-panel-active');
+    }
+
+    signUpButton.addEventListener('click', () => {
+        container.classList.add('right-panel-active');//При клике на кнопку "Sign Up" добавляем класс 
+        localStorage.setItem('rightPanelActive', 'true');//right-panel-active к контейнеру и сохраняем это состояние в localStorage.
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove('right-panel-active'); // При клике на кнопку "Sign In" удаляем класс right-panel-active                                             
+        localStorage.setItem('rightPanelActive', 'false'); // с контейнеру и сохраняем это состояние в localStorage.
+    });
+});
